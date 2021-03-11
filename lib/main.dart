8acc7cb9b76dd4c951c39301ac9ae2dcd8544219
Danh Nhan Phung <nhan.phung@itgrate.com>
2021-03-12 00:09:18 +0100

    add bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -10,58 +10,80 @@ import 'presentations/cocktail_list.dart';
 import 'models/ingredient.dart';
 
 void main() {
-  final initialState = AppState(
-      cocktails: [
-        Cocktail(
-          name: "Gin Tonic",
-          description: "This cocktail is about to be awesome",
-          imageUrl: "https://essenrezept.de/wp-content/uploads/2020/04/Mojito-Rezept.jpg",
-          preparation: "Put it together and shake it yeah",
-          author: "Jane",
-          ingredients: [Ingredient(name: "Gin", amount: "3cl" ), Ingredient(name: "Tonic", amount: "125 ml" )],
-        ),
-        Cocktail(
-          name: "Pinacolada",
-          description: "This cocktail is about to be awesome",
-          imageUrl: "https://www.lecker.de/assets/styles/610x457/public/field/image/cocktails-b15.jpg?itok=WiWEoVFg",
-          preparation: "Put it together and shake it yeah",
-          author: "Jane",
-          ingredients: [Ingredient(name: "Vodka", amount: "3cl" ), Ingredient(name: "Bull", amount: "125 ml" )],
-        ),
-        Cocktail(
-          name: "Test Cocktail",
-          description: "This cocktail is about to be awesome",
-          imageUrl: "https://essenrezept.de/wp-content/uploads/2020/04/Mojito-Rezept.jpg",
-          preparation: "Put it together and shake it yeah",
-          author: "Jane",
-          ingredients: [Ingredient(name: "Gin", amount: "3cl" ), Ingredient(name: "Tonic", amount: "125 ml" )],
-        ),
-        Cocktail(
-          name: "Test Cocktail 2",
-          description: "This cocktail is about to be awesome",
-          imageUrl: "https://www.lecker.de/assets/styles/610x457/public/field/image/cocktails-b15.jpg?itok=WiWEoVFg",
-          preparation: "Put it together and shake it yeah",
-          author: "Jane",
-          ingredients: [Ingredient(name: "Vodka", amount: "3cl" ), Ingredient(name: "Bull", amount: "125 ml" )],
-        ),
-        Cocktail(
-          name: "Test Cocktail",
-          description: "This cocktail is about to be awesome",
-          imageUrl: "https://essenrezept.de/wp-content/uploads/2020/04/Mojito-Rezept.jpg",
-          preparation: "Put it together and shake it yeah",
-          author: "Tom",
-          ingredients: [Ingredient(name: "Gin", amount: "3cl" ), Ingredient(name: "Tonic", amount: "125 ml" )],
-        ),
-        Cocktail(
-          name: "Test Cocktail 2",
-          description: "This cocktail is about to be awesome",
-          imageUrl: "https://www.lecker.de/assets/styles/610x457/public/field/image/cocktails-b15.jpg?itok=WiWEoVFg",
-          preparation: "Put it together and shake it yeah",
-          author: "Tracy",
-          ingredients: [Ingredient(name: "Vodka", amount: "3cl" ), Ingredient(name: "Bull", amount: "125 ml" )],
-        ),
-      ]
-  );
+  final initialState = AppState(cocktails: [
+    Cocktail(
+      name: "Gin Tonic",
+      description: "This cocktail is about to be awesome",
+      imageUrl:
+          "https://essenrezept.de/wp-content/uploads/2020/04/Mojito-Rezept.jpg",
+      preparation: "Put it together and shake it yeah",
+      author: "Jane",
+      ingredients: [
+        Ingredient(name: "Gin", amount: "3cl"),
+        Ingredient(name: "Tonic", amount: "125 ml")
+      ],
+    ),
+    Cocktail(
+      name: "Pinacolada",
+      description: "This cocktail is about to be awesome",
+      imageUrl:
+          "https://www.lecker.de/assets/styles/610x457/public/field/image/cocktails-b15.jpg?itok=WiWEoVFg",
+      preparation: "Put it together and shake it yeah",
+      author: "Jane",
+      ingredients: [
+        Ingredient(name: "Vodka", amount: "3cl"),
+        Ingredient(name: "Bull", amount: "125 ml")
+      ],
+    ),
+    Cocktail(
+      name: "Test Cocktail",
+      description: "This cocktail is about to be awesome",
+      imageUrl:
+          "https://essenrezept.de/wp-content/uploads/2020/04/Mojito-Rezept.jpg",
+      preparation: "Put it together and shake it yeah",
+      author: "Jane",
+      ingredients: [
+        Ingredient(name: "Gin", amount: "3cl"),
+        Ingredient(name: "Tonic", amount: "125 ml")
+      ],
+    ),
+    Cocktail(
+      name: "Test Cocktail 2",
+      description: "This cocktail is about to be awesome",
+      imageUrl:
+          "https://www.lecker.de/assets/styles/610x457/public/field/image/cocktails-b15.jpg?itok=WiWEoVFg",
+      preparation: "Put it together and shake it yeah",
+      author: "Jane",
+      ingredients: [
+        Ingredient(name: "Vodka", amount: "3cl"),
+        Ingredient(name: "Bull", amount: "125 ml")
+      ],
+    ),
+    Cocktail(
+      name: "Test Cocktail",
+      description: "This cocktail is about to be awesome",
+      imageUrl:
+          "https://essenrezept.de/wp-content/uploads/2020/04/Mojito-Rezept.jpg",
+      preparation: "Put it together and shake it yeah",
+      author: "Tom",
+      ingredients: [
+        Ingredient(name: "Gin", amount: "3cl"),
+        Ingredient(name: "Tonic", amount: "125 ml")
+      ],
+    ),
+    Cocktail(
+      name: "Test Cocktail 2",
+      description: "This cocktail is about to be awesome",
+      imageUrl:
+          "https://www.lecker.de/assets/styles/610x457/public/field/image/cocktails-b15.jpg?itok=WiWEoVFg",
+      preparation: "Put it together and shake it yeah",
+      author: "Tracy",
+      ingredients: [
+        Ingredient(name: "Vodka", amount: "3cl"),
+        Ingredient(name: "Bull", amount: "125 ml")
+      ],
+    ),
+  ]);
 
   final store = Store<AppState>(
     appReducer,
@@ -81,13 +103,60 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(fontFamily: 'Rochester'),
       title: 'Ctail App',
       home: Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
+        bottomNavigationBar: BottomNavigationBar(
           backgroundColor: Colors.black,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.favorite,
+                  color: Colors.white,
+                  size: 24,
+                ),
+                label: "Favorite"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.search,
+                  color: Colors.white,
+                  size: 24,
+                ),
+                label: "Favorite"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.wine_bar_rounded,
+                  color: Colors.white,
+                  size: 24,
+                ),
+                label: "Favorite"),
+          ],
         ),
-        body: Padding(
-          padding: EdgeInsets.all(20),
-          child: CocktailList(),
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(100),
+          child: Container(
+            margin: EdgeInsets.only(top: 40),
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 24.0,
+                ),
+                Text(
+                  "Stacy Stanley",
+                  style: TextStyle(
+                      fontFamily: "RockSalt",
+                      fontSize: 12,
+                      color: Colors.white),
+                )
+              ],
+            ),
+          ),
+        ),
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: CocktailList(),
+          ),
         ),
       ),
     );
